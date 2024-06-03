6ded4b1685cb50d55df5737899dc9b5428f85430
Felipe <ftgo.stuff@gmail.com>
2024-06-02 21:36:51 -0400

    first commit

---
+++ ConsoleApp1/MyStandardDeviationSampleExpression.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure.Internal;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Query;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ConsoleApp1;
+
+public class MyStandardDeviationSampleDbFunctionsExtensions
+{
+    public static MethodInfo MyStandardDeviationSampleMethod => typeof(MyStandardDeviationSampleDbFunctionsExtensions).GetMethod(nameof(MyStandardDeviationSample))!;
+
+    public static double MyStandardDeviationSample(IEnumerable<double> values)
+        => throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(MyStandardDeviationSample)));
+}
+
+public static class MyStandardDeviationSampleExtensions
+{
+    public static ModelBuilder AddMyStandardDeviationSampleSupport(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDbFunction(MyStandardDeviationSampleDbFunctionsExtensions.MyStandardDeviationSampleMethod)
+            .HasTranslation(expressions =>
+            {
+                var source = new EnumerableExpression(expressions[0]);
+
+                if (source.Selector is not SqlExpression sqlExpression)
+                    return null;
+
+                return MyStandardDeviationSampleTranslator.AggregateFunction(
+                    "stddev_samp",
+                    [sqlExpression],
+                    source,
+                    nullable: true,
+                    argumentsPropagateNullability: [false],
+                    typeof(double),
+                    expressions[0].TypeMapping);
+            });
+
+        return modelBuilder;
+    }
+
+    public static NpgsqlDbContextOptionsBuilder AddMyStandardDeviationSampleSupportTranslator(this NpgsqlDbContextOptionsBuilder optionsBuilder)
+    {
+        var coreOptionsBuilder = ((IRelationalDbContextOptionsBuilderInfrastructure)optionsBuilder).OptionsBuilder;
+
+        var extension = coreOptionsBuilder.Options.FindExtension<MyStandardDeviationSampleDbContextOptionsExtension>() ?? new MyStandardDeviationSampleDbContextOptionsExtension();
+
+        ((IDbContextOptionsBuilderInfrastructure)coreOptionsBuilder).AddOrUpdateExtension(extension);
+
+        return optionsBuilder;
+    }
+}
+
+public class MyStandardDeviationSampleTranslator : IMethodCallTranslator
+{
+    private readonly IRelationalTypeMappingSource _typeMappingSource;
+    private readonly NpgsqlSqlExpressionFactory _sqlExpressionFactory;
+
+    public MyStandardDeviationSampleTranslator(IRelationalTypeMappingSource typeMappingSource, NpgsqlSqlExpressionFactory sqlExpressionFactory)
+    {
+        _typeMappingSource = typeMappingSource;
+        _sqlExpressionFactory = sqlExpressionFactory;
+
+        //stringTypeMapping = typeMappingSource.FindMapping(typeof(string));
+        //jsonbTypeMapping = typeMappingSource.FindMapping("jsonb");
+    }
+
+    public SqlExpression? Translate(SqlExpression? instance, MethodInfo method, IReadOnlyList<SqlExpression> expressions, IDiagnosticsLogger<DbLoggerCategory.Query> logger)
+    {
+        if (method != MyStandardDeviationSampleDbFunctionsExtensions.MyStandardDeviationSampleMethod)
+            return null;
+
+        var source = new EnumerableExpression(expressions[0]);
+
+        if (source.Selector is not SqlExpression sqlExpression)
+            return null;
+
+        return AggregateFunction(
+            "stddev_samp",
+            [sqlExpression],
+            source,
+            nullable: true,
+            argumentsPropagateNullability: [false],
+            typeof(double),
+            expressions[0].TypeMapping);
+    }
+
+    public static PgFunctionExpression AggregateFunction(
+        string name,
+        IEnumerable<SqlExpression> arguments,
+        EnumerableExpression aggregateEnumerableExpression,
+        bool nullable,
+        IEnumerable<bool> argumentsPropagateNullability,
+        Type returnType,
+        RelationalTypeMapping? typeMapping = null)
+    {
+        var typeMappedArguments = new List<SqlExpression>();
+
+        foreach (var argument in arguments)
+        {
+            typeMappedArguments.Add(ApplyDefaultTypeMapping(argument));
+        }
+
+        return new PgFunctionExpression(
+            name,
+            typeMappedArguments,
+            argumentNames: null,
+            argumentSeparators: null,
+            aggregateEnumerableExpression.IsDistinct,
+            aggregateEnumerableExpression.Predicate,
+            aggregateEnumerableExpression.Orderings,
+            nullable: nullable,
+            argumentsPropagateNullability: argumentsPropagateNullability,
+            type: returnType,
+            typeMapping: typeMapping);
+    }
+
+    public static SqlExpression? ApplyDefaultTypeMapping(SqlExpression? sqlExpression)
+    {
+        return sqlExpression;
+    }
+}
+
+public class MyStandardDeviationSampleTranslatorPlugin : IMethodCallTranslatorPlugin
+{
+    public IEnumerable<IMethodCallTranslator> Translators { get; }
+
+    public MyStandardDeviationSampleTranslatorPlugin(IRelationalTypeMappingSource typeMappingSource, ISqlExpressionFactory sqlExpressionFactory)
+    {
+        Translators = [new MyStandardDeviationSampleTranslator(typeMappingSource, (NpgsqlSqlExpressionFactory)sqlExpressionFactory)];
+    }
+}
+
+public class MyStandardDeviationSampleDbContextOptionsExtension : IDbContextOptionsExtension
+{
+    public DbContextOptionsExtensionInfo Info => new ExtensionInfo(this);
+
+    public void Validate(IDbContextOptions options) { }
+
+    void IDbContextOptionsExtension.ApplyServices(IServiceCollection services)
+        => services.AddSingleton<IMethodCallTranslatorPlugin, MyStandardDeviationSampleTranslatorPlugin>();
+
+    private sealed class ExtensionInfo(IDbContextOptionsExtension extension) : DbContextOptionsExtensionInfo(extension)
+    {
+        private new NpgsqlNodaTimeOptionsExtension Extension
+            => (NpgsqlNodaTimeOptionsExtension)base.Extension;
+
+        public override bool IsDatabaseProvider
+            => false;
+
+        public override int GetServiceProviderHashCode()
+            => 0;
+
+        public override bool ShouldUseSameServiceProvider(DbContextOptionsExtensionInfo other)
+            => true;
+
+        public override void PopulateDebugInfo(IDictionary<string, string> debugInfo)
+            => debugInfo["Npgsql:" + nameof(NpgsqlNodaTimeDbContextOptionsBuilderExtensions.UseNodaTime)] = "1";
+
+        public override string LogFragment
+            => "using NodaTime ";
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,101 @@
+﻿using ConsoleApp1;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+await builder.InitializeAsync();
+
+var host = builder.Build();
+
+await host.InitializeAsync();
+
+await host.RunAsync();
+
+public class FooService(FooContext context) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Console.WriteLine(JsonSerializer.Serialize(await context.Foos.Skip(5).FirstAsync(cancellationToken: stoppingToken)));
+
+        Console.WriteLine(await context.Foos
+            .GroupBy(e1 => e1.IntValue)
+            .Select(g => EF.Functions.StandardDeviationSample(g.Select(e2 => e2.DoubleValue)))
+            .FirstAsync());
+
+        Console.WriteLine(await context.Foos
+            .GroupBy(e1 => e1.IntValue)
+            .Select(g => MyStandardDeviationSampleDbFunctionsExtensions.MyStandardDeviationSample(g.Select(e2 => e2.DoubleValue)))
+            .FirstAsync());
+    }
+}
+
+public static class FooExtensions
+{
+    public static async Task InitializeAsync(this HostApplicationBuilder builder)
+    {
+        await Task.Run(() =>
+        {
+            builder.Services.AddDbContext<FooContext>();
+            builder.Services.AddScoped<FooInitializer>();
+            builder.Services.AddHostedService<FooService>();
+        });
+    }
+
+    public static async Task InitializeAsync(this IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+        await scope.ServiceProvider.GetRequiredService<FooInitializer>().InitializeAsync();
+    }
+}
+
+public class Foo
+{
+    [Key]
+    public long Id { get; set; }
+    public int IntValue { get; set; }
+    public double DoubleValue { get; set; }
+}
+
+public class FooContext : DbContext
+{
+    public DbSet<Foo> Foos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .LogTo(Console.WriteLine)
+            .EnableSensitiveDataLogging()
+            .UseNpgsql("Host=localhost;Database=webapp;Username=backend_migration_user;Password=password",
+                npgsqlBuilder => npgsqlBuilder
+                    .UseNodaTime()
+                    .AddMyStandardDeviationSampleSupportTranslator());
+}
+
+public class FooInitializer
+{
+    private readonly FooContext _context;
+
+    public FooInitializer(FooContext webappContext)
+    {
+        _context = webappContext;
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _context.Database.EnsureCreatedAsync();
+        await _context.Foos.ExecuteDeleteAsync();
+
+        for (int i = 0; i < 10; i++)
+            _context.Foos.Add(new Foo
+            {
+                IntValue = i % 2,
+                DoubleValue = i
+            });
+
+        await _context.SaveChangesAsync();
+    }
+}
